94cf9491b3440a5ae3132417388257ddd4cacec2
Mdmostazir <mdmostazir@outlook.com>
2020-10-01 11:46:53 -0400

    update my loginPage and HomePage

---
+++ src/test/java/com/qa/tests/HomePageTest.java
@@ -0,0 +1,33 @@
+package com.qa.tests;
+
+public class HomePageTest {
+	
+	String username;
+	String password;
+	String emailId;
+	int otp;
+	long mobileNumber;
+	String facebookId;
+
+	public HomePageTest(String username, String password, String emailId, int otp, long mobileNumber, String facebookId) {
+		this.username = username;
+		this.password = password;
+		this.emailId = emailId;
+		this.otp = otp;
+		this.mobileNumber = mobileNumber;
+		this.facebookId = facebookId;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+	public static void main(String[] args) {
+		
+	}
+
+}
--- src/test/java/com/qa/tests/LoginTest.java
+++ src/test/java/com/qa/tests/LoginTest.java
@@ -1,15 +1,47 @@
 package com.qa.tests;
 
-import org.testng.annotations.Test;
-
 public class LoginTest {
 
 	
-	@Test
-	public void test1(){
-		System.out.println("test1");
+	String username;
+	String password;
+	String emailId;
+	int otp;
+	long mobileNumber;
+	String facebookId;
+
+	public LoginTest(String username, String password, String emailId, int otp, long mobileNumber, String facebookId) {
+		this.username = username;
+		this.password = password;
+		this.emailId = emailId;
+		this.otp = otp;
+		this.mobileNumber = mobileNumber;
+		this.facebookId = facebookId;
 	}
+
+	 
 	
+	public void doLogin(){
+		System.out.println("login with: "+ emailId + " " + otp);
+	}
+
+	public static void main(String[] args) {
+
+		 
+		int fact = 1;
+		for(int i = 1; i<=6; i++){
+			fact = fact * i;
+		}
+		
+		System.out.println(fact);
+		
+	}  	
+	
+	
+	
+	
+	 
+
 	
 	
 	
