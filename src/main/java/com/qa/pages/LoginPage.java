3ddbcf7268089777315b70d09f95c7b13f1ea040
Mdmostazir <mdmostazir@outlook.com>
2020-12-13 17:01:05 -0500

    add some method in loginPage

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -11,7 +11,20 @@ public class LoginPage {
 		 
 	 }
 	 
-	
+	 public void setUp(){
+		 System.out.println("this is setUp method");
+	 }
+	 
+	 public void login(){
+ 
+		 System.out.println("This is login method");
+		 
+	 }
+	 
+	 
+	 public void tearDown(){
+		 System.out.println("this is teardown Method");
+	 }
 	
 
 }
